8b5e86fc62ef0c677e24722a0487d533d43e06ea
Shounak Ghosh <crazykavlogs@gmail.com>
2023-04-24 02:32:20 +0530

    center post images

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -3,7 +3,9 @@
 	const { Content, title, date } = data;
 </script>
 
-<article class="prose-lg prose-neutral prose-ul:list-disc text-white p-3 pt-16 max-w-none">
+<article
+	class="prose-lg prose-neutral prose-ul:list-disc prose-img:mx-auto text-white p-3 pt-16 max-w-none"
+>
 	<h1 class="text-center">{title}</h1>
 	<p class="text-center">Published on: {date}</p>
 	<div class="flex flex-col">
