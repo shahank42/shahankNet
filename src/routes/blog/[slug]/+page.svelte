273d7b830a544d359fdd6e3b2234fd3879971e0d
Shounak Ghosh <crazykavlogs@gmail.com>
2023-04-24 12:28:37 +0530

    blog post page now shows tags

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -2,6 +2,8 @@
 	export let data;
 	const { Content, title, subtitle, cover_image, date_published, date_updated, description, tags } =
 		data;
+
+	const tagsList = tags.split(', ');
 </script>
 
 <article
@@ -9,11 +11,20 @@
 >
 	<h1 class="font-ubuntu-mono text-center font-bold">{title}</h1>
 	<h3 class="font-ubuntu-mono text-center text-neutral-200">{subtitle}</h3>
-	<div class="flex flex-col md:flex-row not-prose mt-10 mb-14 md:mt-14">
+	<div class="flex flex-col md:flex-row not-prose mt-10 mb-10 md:mt-14">
 		<p class="text-center font-ubuntu-mono text-sm">published_on: {date_published}</p>
 		<span class="flex-grow" />
 		<p class="text-center font-ubuntu-mono text-sm">updated_on: {date_updated}</p>
 	</div>
+
+	<div class="flex flex-row space-x-5 flex-wrap justify-center">
+		{#each tagsList as tag}
+			<span class="bg-green-600 rounded-full px-4 py-1 mt-4 text-sm">
+				{tag}
+			</span>
+		{/each}
+	</div>
+
 	{#if cover_image !== ''}
 		<img src={cover_image} alt="Cover Image" />
 	{/if}
